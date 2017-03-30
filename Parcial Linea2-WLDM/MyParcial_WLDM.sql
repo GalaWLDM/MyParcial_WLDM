35438ad3e396310a93652531be40c4f589ba97f9
William <wduenas@unac.edu.co>
2017-03-30 10:21:53 -0500

    no message

--- Parcial Linea2-WLDM/MyParcial_WLDM.sql
+++ Parcial Linea2-WLDM/MyParcial_WLDM.sql
@@ -1,1 +1,16 @@
-MyParcial_WLDM.sql
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: consultorio
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+--
+-- Table structure for table `consultorio`
+--
+DROP TABLE IF EXISTS consultorio;
+
+CREATE TABLE paciente(
+idP int,
+nombre varchar(50),
+apellido varchar(50)
+PRIMARY KEY (idP)
+)
